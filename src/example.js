c23fe23145e17761bbfcc0133a53c7743f710480
aw-sxt <alex.williams@spaceandtime.io>
2024-11-13 19:07:21 -0600

    fix jsx component

--- src/example.js
+++ src/example.js
@@ -1,26 +1,20 @@
+
+
 function Counter() {
 	const [count, setCount] = React.useState(0);
-  
+
 	const increment = () => setCount(count + 1);
 	const decrement = () => setCount(count - 1);
-  
-	return React.createElement(
-	  'div',
-	  null,
-	  React.createElement('h1', null, `Counter: ${count}`),
-	  React.createElement(
-		'button',
-		{ onClick: increment },
-		'Increment'
-	  ),
-	  React.createElement(
-		'button',
-		{ onClick: decrement },
-		'Decrement'
-	  )
+
+	return (
+		<div>
+			<h1>Counter: {count}</h1>
+			<button onClick={increment}>Increment</button>
+			<button onClick={decrement}>Decrement</button>
+		</div>
 	);
-  }
-  
-  export {
-	  Counter
-  }
+}
+
+export {
+	Counter
+}
